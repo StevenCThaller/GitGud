bc3c7d43da75b07802d5f46cefa04bfe5b4a25e2
Ryan Alumbaugh <ralumbaugh@gmail.com>
2020-06-15 14:54:12 -0700

    Added the most important line of code in my life

--- Startup.cs
+++ Startup.cs
@@ -18,6 +18,8 @@ namespace GitGud
         {
         }
 
+        // Gotta add this comment in here. It's an incredibly important comment!
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
