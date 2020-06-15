9a06d4ee1f24a24de1c92c6b1e091e3e1f4afdbd
Cody Thaller <stevencthaller@gmail.com>
2020-06-15 14:42:26 -0700

    Wrote a couple of comments

--- Startup.cs
+++ Startup.cs
@@ -21,11 +21,14 @@ namespace GitGud
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Actually, the powerhouse of the cell is the mitochondria
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+
+            // Homer Simpson loves donuts
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
